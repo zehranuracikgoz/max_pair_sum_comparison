60dd5067fa25f0660a6660f6a568c3373a1817ac
zehranuracikgoz <nurzehracik@outlook.com>
2025-01-12 13:49:55 +0300

    Create merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,31 @@
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    middle = len(arr) // 2
+    left = arr[:middle]
+    right = arr[middle:]
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+def result(arr):
+    sorted = merge_sort(arr)
+    last_two_indices = len(sorted) - 2
+    sum_last_two = sorted[last_two_indices] + sorted[last_two_indices + 1]
+    return sum_last_two
